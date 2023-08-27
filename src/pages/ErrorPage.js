a0d1c1d36c1d2a21a6a4113024b92796a239ba1e
Emirlan <abdykadyrov.emirlan.00@gmail.com>
2023-08-27 22:14:03 +0600

    commit

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -9,7 +9,7 @@ const ErrorPage = () => {
         <h1>404</h1>
         {/* <h3>Sorry, the page you tried cannot be found</h3> */}
         <Link to="/" className="btn">
-          {/* back home */}
+          back home
         </Link>
       </section>
     </Wrapper>
